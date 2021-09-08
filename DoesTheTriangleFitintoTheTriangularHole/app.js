1a532d82c0f650c95e22a20acffa5b974680b151
C8147SerkanKilic <81584766+C8147SerkanKilic@users.noreply.github.com>
2021-09-08 23:00:23 +0300

    second

--- DoesTheTriangleFitintoTheTriangularHole/app.js
+++ DoesTheTriangleFitintoTheTriangularHole/app.js
@@ -13,3 +13,21 @@ console.log(doesTriangleFit([1,1,1],[2,2,2]))
 // console.log(doesTriangleFit([3,4,5],[1,1,1]))
 // console.log(doesTriangleFit([1,1,1],[2,2,2]))
 // console.log(doesTriangleFit([1,1,1],[1,3,5]))
+
+
+// --------------------2.yol
+// function doesTriangleFit(a, b) {
+//     function validate(x) {
+//       if (x[0] + x[1] > x[2] && x[0] + x[2] > x[1] && x[2] + x[1] > x[0])
+//         return true;
+//     }
+  
+//     if (validate(a) && validate(b)) {
+//       if (a[0] <= b[0] && a[1] <= b[1] && a[2] <= b[2]) return true;
+//     } else return false;
+//   }
+  
+//   console.log(doesTriangleFit([1, 1, 1], [1, 1, 1]));
+//   console.log(doesTriangleFit([1, 1, 1], [2, 2, 2]));
+//   console.log(doesTriangleFit([1, 2, 3], [1, 2, 2]));
+//   console.log(doesTriangleFit([1, 2, 4], [1, 2, 6]));
