ccbb430f2499349a3fd488215a2e495492a91f98
C8147SerkanKilic <81584766+C8147SerkanKilic@users.noreply.github.com>
2021-09-09 19:23:11 +0300

    cmmt

---
+++ TracktheRobot/app.js
@@ -0,0 +1,8 @@
+//This robot roams around a 2D grid. It starts at (0, 0) facing North. After each time it moves, the robot rotates 90 degrees clockwise. Given the amount the robot has moved each time, you have to calculate the robot's final position.
+// To illustrate, if the robot is given the movements 20, 30, 10, 40 then it will move:
+// 20 steps North, now at (0, 20)
+// 30 steps East, now at (30, 20)
+// 10 steps South. now at (30, 10)
+// 40 steps West, now at (-10, 10)
+// ...and will end up at coordinates (-10, 10).
+
