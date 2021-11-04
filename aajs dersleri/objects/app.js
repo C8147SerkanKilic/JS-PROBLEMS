255ee963e43285a11503c5f6888c517e9e61f763
C8147SerkanKilic <81584766+C8147SerkanKilic@users.noreply.github.com>
2021-11-04 20:40:30 +0300

    inclass

--- aajs dersleri/objects/app.js
+++ aajs dersleri/objects/app.js
@@ -21,3 +21,31 @@ console.log(person['2021']);
 
 const field = 'Name';
 console.log(person['first' + field]); */
+const person = {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+    citiesLived: ['New York', 'Paris', 'London'],
+    location: {
+      city: 'New York',
+      temp: 92,
+    },
+    2021: 'Clarusway',
+    getFullName: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    getFullName2() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    aMethod: () => {
+      return 'Hello';
+    },
+    printCitiesLived() {
+      this.citiesLived.forEach(city => {
+        console.log(this.firstName + ' has lived in ' + city);
+      });
+    },
+  };
+  // console.log(person.getFullName2());
+  
+  person.printCitiesLived();
