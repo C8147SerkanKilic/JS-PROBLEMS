526b5e7f9610a69310749241bf8cb430a239df3c
C8147SerkanKilic <81584766+C8147SerkanKilic@users.noreply.github.com>
2021-11-15 20:27:11 +0300

    inclass

---
+++ aajs dersleri/asnc/app.js
@@ -0,0 +1,35 @@
+// setTimeout(() => {
+//   console.log('1. merhaba');
+//   setTimeout(() => {
+//     console.log('2. nasilsin?');
+//     setTimeout(() => {
+//       console.log('3. görüşmek üzere');
+//     }, 0);
+//   }, 0);
+// }, 0);
+
+setTimeout(() => {
+    console.log('1');
+  }, 0);
+  setTimeout(() => {
+    console.log('2');
+  }, 0);
+  setTimeout(() => {
+    console.log('3');
+  }, 0);
+  setTimeout(() => {
+    console.log('4');
+  }, 0);
+  // console.log('1. merhaba');
+  // console.log('2. nasilsin?');
+  // console.log('3. görüşmek üzere');
+  
+  // let i = 0;
+  // let counter = setInterval(() => {
+  //   i++;
+  //   console.log(`${i} seconds passed`);
+  //   if (i == 10) clearInterval(counter);
+  // }, 1000);
+
+
+///setTimeout iç içe yazılırsa birinciden sonra ikinci ama alt alta yazılırsa bekleme sürelerinin uzunluğu ayarlanmalı
