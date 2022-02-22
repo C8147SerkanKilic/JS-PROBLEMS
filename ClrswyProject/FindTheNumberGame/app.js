d3f03e4a5fb549333c04c4920c685b8bbdd3eac6
C8147SerkanKilic <81584766+C8147SerkanKilic@users.noreply.github.com>
2022-02-23 01:36:09 +0300

    s

---
+++ ClrswyProject/FindTheNumberGame/app.js
@@ -0,0 +1,13 @@
+const prg = document.getElementById("textt")
+const btn = document.getElementById("button")
+const def = document.getElementById("abc")
+
+// const x = Math.floor(Math.random()*100);
+
+// btn.addEventListener("click",function () {
+//     const input = Number(document.getElementById("input").value)
+//          if (input == x) { 
+//                 prg.innerHTML="Tebrikler"}
+//             else {
+//                 prg.innerHTML="Sorry,Try again later";}})
+def.innerHTML = "asdasdasdasd";
