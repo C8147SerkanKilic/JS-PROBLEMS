7b4239e306ce7a612b0862dd6b849bade4539441
C8147SerkanKilic <81584766+C8147SerkanKilic@users.noreply.github.com>
2022-02-25 21:51:04 +0300

    s

---
+++ Edabit/Learn Lodash; Write Your Own Version of Partition, Split an Array in Two/app.js
@@ -0,0 +1,5 @@
+// Learn Lodash: Write Your Own Version of Partition, Split an Array in Two
+
+// Write your own version of the lodash _.partition function. Partition receives a "collection", which can be an array or object, and a function that will check each item in the collection, it returns an array containing two arrays, one of the values that passed the check and the other with values that didn't.
+
+
